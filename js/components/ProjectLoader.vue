a745fd95ae26663347abf0b2df474271f6348d9f
Artemiy Solopov <art-solopov@yandex.ru>
2024-08-04 17:06:21 +0300

    Project loader fix

--- js/components/ProjectLoader.vue
+++ js/components/ProjectLoader.vue
@@ -3,19 +3,22 @@
     import { useStore } from '@nanostores/vue'
 
     import { $projects } from '../stores/projects'
-    import { $currentProjectKey } from '../stores/current_project'
+    import { $currentProjectKey, setProject } from '../stores/current_project'
 
     const projects = useStore($projects)
     const currentProject = useStore($currentProjectKey)
     
     const isShown = computed(() => !!currentProject.value);
+
+    function updateProject(event) {
+        const project = event.target.value
+        setProject(project)
+    }
 </script>
 
 <template>
     <div class="field suffix round border" :class="{ 'is-hidden': !isShown }">
-        <select name="project"
-            hx-get="/tasks" hx-trigger="change" hx-push-url="true"
-            hx-target="#top_content" hx-select="#top_content" hx-swap="outerHTML">
+        <select name="project" @change="updateProject">
             <option v-for="project in projects" :value="project._key" :selected="project._key == currentProject" >{{ project.name }}</option>
         </select>
         <i>arrow_drop_down</i>
--- js/stores/current_project.js
+++ js/stores/current_project.js
@@ -10,14 +10,30 @@ function loadKeyFromTopContent() {
     $currentProjectKey.set(currentProject)
 }
 
+function loadKeyFromState(ev) {
+    $currentProjectKey.set(ev.state.project)
+}
+
 onMount($currentProjectKey, () => {
     loadKeyFromTopContent()
     document.addEventListener('htmx:afterSettle', loadKeyFromTopContent)
+    window.addEventListener('popstate', loadKeyFromState)
 
     return () => {
         document.removeEventListener('htmx:afterSettle', loadKeyFromTopContent)
+        window.removeEventListener('popstate', loadKeyFromState)
     }
 })
+
+export function setProject(projectKey) {
+    const prevProject = $currentProjectKey.get()
+    history.replaceState({project: prevProject}, "")
+
+    const url = new URL(window.location)
+    url.searchParams.set('project', projectKey)
+    $currentProjectKey.set(projectKey)
+    history.pushState({project: projectKey}, "", url)
+}
 
 async function _reloadCurrentProject(key) {
     let projectData = null
