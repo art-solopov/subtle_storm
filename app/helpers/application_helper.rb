25f8e3ad422b1883b765a69cce7bcfa1ce3deed3
Artemiy Solopov <a.solopov.001@gmail.com>
2025-10-21 20:52:04 +0300

    Added the path for project selector

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def path_for_projects_selector(project)
+    return project_path(project) if controller_name == 'projects'
+
+    url_for(controller: controller_name, action: :index, project:)
+  end
 end
