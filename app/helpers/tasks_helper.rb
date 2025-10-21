d4551c73631a24ec71e17a5de1adfca8b48afcb1
Artemiy Solopov <a.solopov.001@gmail.com>
2025-10-21 20:36:31 +0300

    Minor restyling

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module TasksHelper
+  def tasks_index_title
+    return "Tasks for #{current_project.name}" if current_project
+
+    # TODO: add conditions for filters, etc
+
+    'Tasks'
+  end
 end
